4e63098a2da63520d57270d9513693d764fffc06
Claus Smitt <claus.smitt@gmail.com>
2024-04-22 16:42:10 +0200

    Update get_bup20.sh
    rename bup20 folder after downloading to have the same name as the mask2former predictions

--- scripts/get_bup20.sh
+++ scripts/get_bup20.sh
@@ -9,4 +9,6 @@ wget -O BUP20.tar.gz -c https://uni-bonn.sciebo.de/s/dbETJWamSqyCYm5/download
 echo Extracting dataset...
 tar -xf BUP20.tar.gz --checkpoint=.10000
 
+mv CKA_sweet_pepper_2020_summer BUP_20
+
 cd ../..
